f2e1f98008353a5cecbbcc7b74b58d6d7c635aa6
rsemenyshyn <ripka.ua@gmail.com>
2021-02-21 22:43:21 +0100

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,8 @@
+<?php
+require_once "src/TSP.php";
+require_once "src/WeightedEdge.php";
+require_once "src/Algorithms/AlgorithmInterface.php";
+require_once "src/Algorithms/BruteForce.php";
+require_once "src/Algorithms/BruteForceGenerator.php";
+require_once "src/Algorithms/CheapestLink.php";
+require_once "src/Algorithms/NearestNeighbour.php";
